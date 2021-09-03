ff2e9b1af108280421f383ca1708987f614c4565
yusuf <mugaluyusu35@gmail.com>
2021-09-03 12:16:52 +0300

    Added static settings file

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_URL= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
 
 LOGIN_REDIRECT_URL = '/quiz'
 LOGOUT_REDIRECT_URL = '/home'
